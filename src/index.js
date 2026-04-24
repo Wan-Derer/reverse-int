10c02595360d9610248c4fab120155195da54573
Wan-Derer <frd-ru@yandex.ru>
2026-04-25 00:50:04 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-module.exports = function reverse(/* n */) {
-  throw new Error('Not implemented');
+module.exports = function reverse(n) {
+  let nn = Math.abs(n);
+  let res = 0;
+
+  while (nn > 0) {
+    res = res * 10 + (nn % 10);
+    nn = Math.floor(nn / 10);
+  }
+
+  return res;
 };
